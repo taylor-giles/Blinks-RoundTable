3177c98d9c71aab1a162fa1a31dbb07d6cbe297b
taylor-giles <taylor.g1823@gmail.com>
2022-01-28 20:29:53 -0500

    Start initBoard

--- Blinks-RoundTable.ino
+++ Blinks-RoundTable.ino
@@ -1,3 +1,11 @@
+const byte BOARD_INIT = 100; //The starting value for board initialization
+struct BOARD_PIECE {
+  byte num;
+  byte nextPiece;
+  byte prevPiece;
+  Color color;
+}
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -8,6 +16,19 @@ void loop() {
 
 }
 
-void initBoard(){
-  
+/**
+ * Initializes
+ */
+void initBoard(byte value){
+  nextPiece = -1;
+  prevPiece = -1;
+
+  //Find the next piece
+  FOREACH_FACE(f){
+    //Check if this face is connected to another Blink
+    if(!isValueReceivedOnFaceExpired(f)){
+      nextPiece = f;
+      setValueSentOnFace(value + 1);
+    }
+  }
 }
